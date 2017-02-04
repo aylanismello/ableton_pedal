0ad92e2f882b5dbbd12a77aa532973809ad8803e
Aylan Mello <aylanismello@gmail.com>
2017-02-04 14:53:47 -0800

    broken

--- switch_arduino.js
+++ switch_arduino.js
@@ -1,20 +1,42 @@
 const five = require("johnny-five"),
 	board = new five.Board();
 
-const pinNumber = 12;
+const MidiTest = require('./midi_test.js');
+const pinNumbers = [8, 10, 12];
+const pinNumber = pinNumbers[2];
+
+let switches = {};
 
 board.on("ready", () => {
 	const mySwitch = new five.Switch(pinNumber);
-	const led = new five.Led(13);
 
-  mySwitch.on("open", () => {
+	mySwitch.on("open", () => {
 		console.log('switch' + pinNumber + ' is open');
-    led.off();
-  });
+		// MidiTest.triggerMidiOn(pinNumber);
+	});
 
-  mySwitch.on("close", () => {
-		console.log('switch' + pinNumber + 'is closed' );
-    led.on();
-  });
+	mySwitch.on("close", () => {
+		console.log('switch' + pinNumber + ' is closed');
+		// MidiTest.triggerMidiOn(pinNumber);
+	});
+
+
+
+
+	// pinNumbers.forEach((pinNumber) => {
+	// 	switches[pinNumber] = new five.Switch(pinNumber);
+	//
+		// switches[pinNumber].on("open", () => {
+		// 	console.log('switch' + pinNumber + ' is open');
+		// 	// MidiTest.triggerMidiOn(pinNumber);
+	  // });
+		//
+		// switches[pinNumber].on("close", () => {
+		// 	console.log('switch' + pinNumber + ' is closed');
+		// 	// MidiTest.triggerMidiOn(pinNumber);
+	  // });
+		//
+	// });
+
 
 });
